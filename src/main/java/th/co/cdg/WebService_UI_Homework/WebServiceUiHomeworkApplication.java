5316b120f38cace737bd6187081108fc1721230b
BooPassakorn <t.passakorn4999@gmail.com>
2025-03-06 16:25:34 +0700

    Initial commit

---
+++ src/main/java/th/co/cdg/WebService_UI_Homework/WebServiceUiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package th.co.cdg.WebService_UI_Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceUiHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceUiHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/co/cdg/WebService_UI_Homework/WebServiceUiHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.cdg.WebService_UI_Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceUiHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
